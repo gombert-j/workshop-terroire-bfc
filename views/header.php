a5ace111e9cd289b3fa0d3efacbe54b5b2d59802
June" GOMBERT <gombertjuno@outlook.com>
2023-10-03 14:55:42 +0200

    Maj 1

---
+++ views/header.php
@@ -0,0 +1,13 @@
+<a href="https://www.yonne.fr"><img src="public/images/yonne.png"></a>
+        <div class="navbar">
+            <ul>
+                <li><a href="index.html">Accueil</a></li>
+                <li><a href="index.html">Liste des domaines</a></li>
+                <li><a href="index.html">Suggérer un domaine</a></li>
+                <li><a href="index.html">Carte</a></li>
+
+            </ul>
+
+        </div>
+
+
---
+++ views/template.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="public/css/styles.css">
+  <title>Terroire de l'Yonne</title>
+</head>
+<body>
+    <main>
+        <div class="header">
+            <?php require('views/header.php'); ?>
+        </div>
+        <div class="title" >
+            <h1><?=$title?></h1>
+        </div>
+
+        <div id="container-1">
+            <?= $content ?>
+        </div>
+        <div class="footer">
+          <label>Crédits: Les icônes utilisées proviennent du site WEB: <a href='https://icons.getbootstrap.com/'>https://icons.getbootstrap.com/</a> sous accord de la licence creative commons.</label>
+        </div>
+    </main>
+
+</body>
+</html>
