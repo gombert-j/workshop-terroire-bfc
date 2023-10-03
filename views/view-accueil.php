86de86b50a3f9f15c5bc26dba9a3b96d8aa3a474
June" GOMBERT <gombertjuno@outlook.com>
2023-10-03 16:15:53 +0200

    MàJ MVC 2

--- views/view-accueil.php
+++ views/view-accueil.php
@@ -1,4 +1,4 @@
-<?php ob_start() ?>
+
             <button class="accueil-box">
               <div class="accueil-box-elem"><img src="public/icons/postage-heart-fill.svg" alt="Logo de la liste des domaines" width="200vh" height="200vh"></div>
               <h1 class="accueil-box-elem">Liste des domaines</h1>
@@ -8,9 +8,3 @@
               <div class="accueil-box-elem"><img src="public/icons/map-fill.svg" alt="Logo de la carte des domaines" width="200vh" height="200vh"></div>
               <h1 class="accueil-box-elem">Carte des domaines</h1>
             </button>
-
-        </div>
-<?php 
-$content =  ob_get_clean();
-require('views/template.php');
-
