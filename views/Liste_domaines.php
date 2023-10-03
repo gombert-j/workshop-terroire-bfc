5823d7fc0cc10ba099e73e38594e9ffea8356362
Mael58 <matzenerm@gmail.com>
2023-10-03 16:26:45 +0200

    ajout liste

---
+++ views/Liste_domaines.php
@@ -0,0 +1,39 @@
+<section class="body">
+    <div class="card">
+        <div class="header_card">Domaine a rechercher</div>
+        <div class="body_card">
+            <div class="filtre">
+
+                <ul>
+                    <li>
+                        <input type="checkbox"> Vin
+                    </li>
+                    <li>
+                        <input type="checkbox"> Fromage
+                    </li>
+                    <li>
+                        <input type="checkbox"> Escargot
+                    </li>
+
+                </ul>
+
+
+
+            </div>
+            <h3>Distance</h3>
+        </div>
+</section>
+
+
+
+
+
+
+<section class="recherche">
+    <div class="card_Liste">dqq
+
+
+    </div>
+
+
+</section>
--- views/header.php
+++ views/header.php
@@ -1,13 +1,11 @@
 <a href="https://www.yonne.fr"><img src="public/images/yonne.png"></a>
-        <div class="navbar">
-            <ul>
-                <li><a href="index.html">Accueil</a></li>
-                <li><a href="index.html">Liste des domaines</a></li>
-                <li><a href="index.html">Suggérer un domaine</a></li>
-                <li><a href="index.html">Carte</a></li>
+<div class="navbar">
+    <ul>
+        <li><a href="controllers/controller-accueil.php">Accueil</a></li>
+        <li><a href="controllers/controller-Liste.php">Liste des domaines</a></li>
+        <li><a href="index.html">Suggérer un domaine</a></li>
+        <li><a href="index.html">Carte</a></li>
 
-            </ul>
+    </ul>
 
-        </div>
-
-
+</div>
