9a292d04eaf77392b9e1d0b5b13123e6c69b422f
June" GOMBERT <gombertjuno@outlook.com>
2023-10-04 16:03:11 +0200

    Equipe + styles

---
+++ controllers/controller-equipe.php
@@ -0,0 +1,14 @@
+<?php
+//FONCTIONS PHP SUPPLEMENTAIRES SI BESOIN (Par ex: Recherche d'entrées dans la base de données)
+
+
+/////////////////////////////////////////////////////////////// AFFICHAGE DE LA PAGE
+ob_start(); //Active le buffer
+require("../views/view-equipe.php"); //(Dans le Buffer) Mets le contenu de views/view-accueil.php
+$content = ob_get_clean(); //Mets le contenu du buffer dans $content
+
+require('../views/template.php'); //Appel du template (qui utilise $content)
+
+
+
+?>
