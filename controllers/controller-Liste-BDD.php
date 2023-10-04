dfb86d00185a2e2e3c28bd59944ada97958bd257
Mael58 <matzenerm@gmail.com>
2023-10-04 10:28:00 +0200

    ajout requete fromage et escargot

--- controllers/controller-Liste-BDD.php
+++ controllers/controller-Liste-BDD.php
@@ -9,6 +9,7 @@ try {
         $user,
         $mdp
     );
+
     print('connection reussi');
 } catch (Exception $e) {
     die('erreur');
@@ -17,9 +18,48 @@ $sqlQuery = 'SELECT * FROM vin';
 $donnees = $db->prepare($sqlQuery);
 $donnees->execute();
 $recipes = $donnees->fetchAll();
+echo 'Vin';
 foreach ($recipes as $recipe) {
 ?>
     <p><?php echo $recipe['nom']; ?><br>
+        <?php echo $recipe['adresse']; ?><br>
+        <?php echo $recipe['contact']; ?><br>
+        <?php echo $recipe['horaire']; ?><br>
+        <?php echo $recipe['site']; ?><br>
+
+
+    </p>
+<?php
+}
+
+$sqlQuery = 'SELECT * FROM fromage';
+$donnees = $db->prepare($sqlQuery);
+$donnees->execute();
+$recipes = $donnees->fetchAll();
+echo 'Fromage :';
+foreach ($recipes as $recipe) {
+?>
+    <p>
+        <?php echo $recipe['nom']; ?><br>
+        <?php echo $recipe['adresse']; ?><br>
+        <?php echo $recipe['contact']; ?><br>
+        <?php echo $recipe['horaire']; ?><br>
+        <?php echo $recipe['site']; ?><br>
+
+
+    </p>
+<?php
+}
+
+$sqlQuery = 'SELECT * FROM escargot';
+$donnees = $db->prepare($sqlQuery);
+$donnees->execute();
+$recipes = $donnees->fetchAll();
+echo 'Escargot :';
+foreach ($recipes as $recipe) {
+?>
+    <p>
+        <?php echo $recipe['nom']; ?><br>
         <?php echo $recipe['adresse']; ?><br>
         <?php echo $recipe['contact']; ?><br>
         <?php echo $recipe['horaire']; ?><br>
